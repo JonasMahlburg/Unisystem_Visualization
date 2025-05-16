ce88b7c5fe07cb96d354951ba75fd324cf28064a
JonasMahlburg <darrylmanga@yahoo.com>
2025-05-16 14:11:57 +0200

    first commit

---
+++ uni_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UniSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'uni_system'
---
+++ uni_system/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+class Semester(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Prof(models.Model):
+    name = models.CharField(max_lenght=30)
+   
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    course = models.ManyToManyField('Course', related_name='students')
+
+    def __str__(self):
+        return f"{self.name}, {self.course}"
+
+
+class Course_Description(models.Model):
+    description = models.CharField(max_lenght=300)
+    
+    def __str__(self):
+        return f"This course is about: {self.description}"
+
+
+class Student_ID(models.Model):
+    card_number = models.FloatField(max_length=15, unique=True)
+    student = models.OneToOneField(Student, on_delete=models.CASCADE, related_name='id_card')
+        
+    def __str__(self):
+        return f"ID Number {self.card_number} for:{self.student}"
+
+
+
+class Course(models.Model):
+    title = models.CharField(max_lenght=50)
+    description = models.OneToOneField(Course_Description, on_delete= models.CASCADE, related_name='course')
+    semester = models.ForeignKey(Semester, on_delete= models.CASCADE, related_name='course')
+    professor = models.ForeignKey(Prof, on_delete= models.CASCADE, related_name='course' )
+
+    def __str__(self):
+        return f"{self.title}, {self.semester}"
